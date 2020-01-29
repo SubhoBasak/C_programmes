3d47ebae5eeb09d0fff04e9559abf08a2748fc78
SubhoBasak <subhobasak50@gmail.com>
2020-01-29 10:04:50 +0530

    added diamond and triangle pattern maker

---
+++ diamond_pattern.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int i, j, h;
+
+    printf("Enter the height of the tree : ");
+    scanf("%d", &h);
+
+    for(i = 0; i < h; i++){
+        for(j = 0; j < h-i-1; j++){
+            printf(" ");
+        }
+        for(j = 0; j < 2*i+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    h--;
+    for(i = 0; i < h; i++){
+        for(j = 0; j < i+1; j++){
+            printf(" ");
+        }
+        for(j = 0; j < 2*h-1-2*i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
